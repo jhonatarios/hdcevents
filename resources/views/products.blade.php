c1c48eaf557e2109672b23d159db22fb1d822fff
Jhonata Rios <72048481+jhonatarios@users.noreply.github.com>
2022-08-18 21:55:37 -0300

    aula 8

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,2 +1,13 @@
+@extends('layouts.main')
+
+@section('title', 'Produtos')
+
+@section('content')
+
 <h1>Tela de produtos</h1>
 <a href="/">Voltar para a home</a>
+@if($busca != '')
+<p>O usuario está buscando por {{ $busca }}</p>
+@endif
+
+@endsection
